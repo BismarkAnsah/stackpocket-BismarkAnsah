481b03ada31fff220ff09fd6f1d472f820b37d3a
BismarkAnsah <57173824+BismarkAnsah@users.noreply.github.com>
2023-06-06 15:48:34 +0100

    updated

---
+++ updated_heads_tails.php
@@ -0,0 +1,23 @@
+function headsAndTails($luckyNumber, $selections)
+{
+    $paddedNumber = str_pad($luckyNumber, 2, '0', STR_PAD_LEFT);
+    $outcomes = [];
+    $headsSelections = $selections[0];
+    $tailsSelections = $selections[1];
+
+    // Extract the tens and ones positions
+    $heads = (int)($paddedNumber / 10);
+    $tails = $paddedNumber % 10;
+    $headsWon = in_array($heads, $headsSelections);
+    $tailsWon = in_array($tails, $tailsSelections);
+    $outcomes[] =  $headsWon ? 'heads Won' : 'heads Lost';
+    $outcomes[] =  $tailsWon ? 'tails Won' : 'tails Lost';
+    if (!$headsWon && !$tailsWon) {
+        $outcomes = [];
+        $outcomes[] = 'both lost';
+    }else if($headsWon && $tailsWon){
+        $outcomes = [];
+        $outcomes[] = 'both won';
+    }
+    return $outcomes;
+}
